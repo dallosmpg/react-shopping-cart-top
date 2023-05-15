3de912036a9a54a68913265de0df52230d0e754a
Martin Dallos <tinypin14@gmail.com>
2023-05-15 17:07:06 +0200

    Created the render test of the Home component

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,6 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
-// import "@testing-library/jest-dom";  // optional
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import { BrowserRouter } from 'react-router-dom';
 // import userEvent from "@testing-library/user-event";
-import Home from "./Home";
+import Home from "./Home.js";
 
+describe('Home component', () => {
+    it('renders correctly', () => {
+        const setSvgColor = jest.fn();
+
+        render(
+        <BrowserRouter>
+            <Home setSvgColor={setSvgColor} />
+        </BrowserRouter>
+        )
+        const textElement = screen.getByText(/Welcome to the Beanery/i);
+        expect(textElement).toBeInTheDocument();
+      })
+;})
