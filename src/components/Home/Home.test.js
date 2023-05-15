32fe75cfb4a9711c2f08e5afdf2ca23b722c2ceb
Martin Dallos <tinypin14@gmail.com>
2023-05-15 16:39:19 +0200

    Start test setup

---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "@testing-library/react";
+// import "@testing-library/jest-dom";  // optional
+// import userEvent from "@testing-library/user-event";
+import Home from "./Home";
+
