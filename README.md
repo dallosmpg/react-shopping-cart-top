070376c7a214490c15b141629161161ea76fe2e9
Martin Dallos <tinypin14@gmail.com>
2023-05-19 14:51:55 +0200

    Created up-to-date tests for Nav

--- README.md
+++ README.md
@@ -11,12 +11,14 @@ I might only need state for the shopping cart.
 
 ### Planned dev process
 1. Lay out the page statically with every component
+   1. Im creating styling as well so i know in place what i need to do, so i dont have to remember later
+   2. I decided to write tests as writing out basic components as well
 
 
 ## Devlog
 ### Problems and solutions 
-
-### Thoughtprocess
+1. I need to relearn testing as i have gotten quite unfamiliar with it since i last had to test in TOP
+   1. Im trying to use chatGPT as much as possible to compensate
 
 ### Log
 2023.05.08.: Planning, file and component creation
--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -2,8 +2,17 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";  // optional
 import { BrowserRouter } from 'react-router-dom';
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
+import { act } from 'react-dom/test-utils';
 import Nav from "./Nav.js";
+import App from '../../App.js';
+
+async function resetToHomePage() {
+    const user = userEvent.setup();
+    await act(async () => {
+        await user.click(screen.getByRole('link', {name: 'Home'}));
+    })
+};
 
 describe('Nav component', () => {
     it('Nav component renders correctly', () => {
@@ -15,5 +24,44 @@ describe('Nav component', () => {
         const textElement = screen.getByText(/The Beanery Store/i);
         expect(textElement).toBeInTheDocument();
       })
+
+    it('Switches to Shop when shop nv-btn is clicked', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        expect(screen.getByText('New arrivals!')).toBeInTheDocument();
+        expect(screen.getByRole('link', {name: 'Shop'})).toBeInTheDocument();
+        await act(async () => {
+            await user.click(screen.getByRole('link', {name: 'Shop'}));
+        })
+        expect(screen.getByText('Hello from Shop')).toBeInTheDocument();
+
+        await act(async () => {
+            await user.click(screen.getByRole('link', {name: 'Home'}));
+        })    
+    })
+
+    it('Switches to About when about navLink is clicked', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        expect(screen.getByText('New arrivals!')).toBeInTheDocument();
+        expect(screen.getByRole('link', {name: 'About'})).toBeInTheDocument();
+        await act(async () => {
+            await user.click(screen.getByRole('link', {name: 'About'}));
+        })
+        expect(screen.getByText('Hello from About')).toBeInTheDocument();
+
+        await act(async () => {
+            await user.click(screen.getByRole('link', {name: 'Home'}));
+        })    
+    })
+
+    it('Returns to Home page after navigated to Home', () => {
+        render(<App />);
+
+        expect(screen.getByText('New arrivals!')).toBeInTheDocument();
+        expect(screen.getByRole('link', {name: 'About'})).toBeInTheDocument();
+    })
 
 ;})
