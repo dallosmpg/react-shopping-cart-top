f467c8d281546930a431b09857a7c2c73dca0b5d
Martin Dallos <tinypin14@gmail.com>
2023-06-15 18:57:15 +0200

    Completed About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,6 +7,18 @@ export default function About({setSvgColor}) {
     }, []);
 
     return (
-        <h1>Hello from About</h1>
+        <div className="about flex-center-column">
+            <div className="summary flex-center">
+                <p>Hi! I'm Martin. I studied psychology at Budapest, but after graduating i felt i would like to do something else. I decided to try out coding and i stumbled upon The Odin Project. This little front-end project for a webshop has been made as part of the TOP curriculum.</p>
+            </div>
+            <div className="links flex-center">
+                <a href="https://github.com/dallosmpg" target="_blank" rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg"  height="32" viewBox="0 0 1024 1024"><path fill="currentColor" d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5C64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9c26.4 39.1 77.9 32.5 104 26c5.7-23.5 17.9-44.5 34.7-60.8c-140.6-25.2-199.2-111-199.2-213c0-49.5 16.3-95 48.3-131.7c-20.4-60.5 1.9-112.3 4.9-120c58.1-5.2 118.5 41.6 123.2 45.3c33-8.9 70.7-13.6 112.9-13.6c42.4 0 80.2 4.9 113.5 13.9c11.3-8.6 67.3-48.8 121.3-43.9c2.9 7.7 24.7 58.3 5.5 118c32.4 36.8 48.9 82.7 48.9 132.3c0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9c177.1-59.7 304.6-227 304.6-424.1c0-247.2-200.4-447.3-447.5-447.3z"/></svg>
+                </a>
+                <a href="https://www.linkedin.com/in/martin-dallos-843243170/" target="_blank" rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="-2 -2 24 24"><g fill="currentColor"><path d="M15 11.13v3.697h-2.143v-3.45c0-.866-.31-1.457-1.086-1.457c-.592 0-.945.398-1.1.784c-.056.138-.071.33-.071.522v3.601H8.456s.029-5.842 0-6.447H10.6v.913l-.014.021h.014v-.02c.285-.44.793-1.066 1.932-1.066c1.41 0 2.468.922 2.468 2.902zM6.213 5.271C5.48 5.271 5 5.753 5 6.385c0 .62.466 1.115 1.185 1.115h.014c.748 0 1.213-.496 1.213-1.115c-.014-.632-.465-1.114-1.199-1.114zm-1.086 9.556h2.144V8.38H5.127v6.447z"></path><path d="M10 18a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm0 2C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10s-4.477 10-10 10z"></path></g></svg>
+                </a>
+            </div>
+        </div>
     )
 }
